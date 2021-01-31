780310a80c29c52b2f9f6c7bd50ed4ad511495a5
Jack Ren <bjrjk@qq.com>
2021-01-31 14:47:20 +0800

    Update crawler.sh

--- crawler.sh
+++ crawler.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 tg_crawler.py $TG_API_ID $TG_API_HASH $TG_PHONE $TG_DB_ENCRYPTION_KEY
+PYTHONIOENCODING=utf-8 python3 tg_crawler.py $TG_API_ID $TG_API_HASH $TG_PHONE $TG_DB_ENCRYPTION_KEY
